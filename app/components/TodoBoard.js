9ecb8983366692fc9c9ad941b2eb8e0ae0a93d0d
designerDK <tupacalypse@naver.com>
2024-05-25 14:47:22 +0900

    v1.0.0

--- app/components/TodoBoard.js
+++ app/components/TodoBoard.js
@@ -5,7 +5,6 @@ function TodoBoard(props){
 
     return(
         <div>
-            <h1>디자이너디케이 투두리스트 테스트입니다.</h1>
             {props.todoList.map((item, index)=><TodoItem key={index} item={item} />)}
         </div>
     )
--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <title>디자이너디케이 투두리스트 테스트입니다.</title>
+      <title>디자이너디케이 투두리스트 테스트입니다. dk's todo list</title>
       <meta name="description" 
-      content="디자이너디케이 투두리스트 테스트입니다."></meta>
+      content="디자이너디케이 투두리스트 테스트입니다. dk's todo list"></meta>
       <meta name="google-site-verification" content="ohrGnA7RsHT30X375ixKOmz7HJIZHM3ZBCPU3Y2ootg" />
       <meta name="naver-site-verification" content="e3a247e584e22522a6693436645ab33639a09cbf" />
       </head>
-      <body>{children}</body>
+      <body>
+        <h1>디자이너디케이 투두리스트 테스트입니다.</h1>
+        {children}
+      </body>
     </html>
   );
 }
