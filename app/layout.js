b1e29500194c1edf8b25ffa5abeae4b24e672ad0
designerDK <tupacalypse@naver.com>
2024-05-25 12:59:03 +0900

    v1.0.0

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ export default function RootLayout({ children }) {
       <meta name="description" 
       content="디자이너디케이 투두리스트 테스트입니다."></meta>
       <meta name="google-site-verification" content="ohrGnA7RsHT30X375ixKOmz7HJIZHM3ZBCPU3Y2ootg" />
+      <meta name="naver-site-verification" content="e3a247e584e22522a6693436645ab33639a09cbf" />
       </head>
       <body>{children}</body>
     </html>
